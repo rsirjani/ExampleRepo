8cb869271f5674193ffff32f6cf3dd9aed3b5dff
ramti <rsirjani@uwo.ca>
2023-02-17 14:52:30 -0500

    Created new project. Made repository

---
+++ src/examplemap/ExampleMap.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examplemap;
+
+/**
+ *
+ * @author ramti
+ */
+public class ExampleMap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
